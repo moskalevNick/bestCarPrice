601ff575463c36dc25f44d68abde0b95bcbbc511
moskalevNick <moskalevnikolay1@gmail.com>
2025-11-26 16:14:30 +0300

    url to env

--- src/bot/callbacks/testReq.ts
+++ src/bot/callbacks/testReq.ts
@@ -1,15 +1,20 @@
 import axios from 'axios'
+import * as process from "node:process";
 
 export const testReq = async (ctx) => {
     await ctx.answerCallbackQuery();
 
     try {
-        const response = await axios.get('https://api.av.by/offer-types/cars/landings/filter', {
+        const response = process.env.TEST_URL ? await axios.get(process.env.TEST_URL, {
+            headers: {
+                "User-Agent": 'E-power'
+            }
+        }) : await axios.get('https://api.av.by/offer-types/cars/landings/filter', {
             headers: {
                 "User-Agent": 'E-power'
             }
         });
-        await ctx.reply(`Запрос успешен! \n ${JSON.stringify(response.data)}`);
+        await ctx.reply(`Запрос успешен c ${process.env.TEST_URL ? 'process.env.TEST_URL' : 'default url'}! \n ${JSON.stringify(response.data)}`);
     } catch (error) {
         await ctx.reply(`Ошибка при выполнении запроса: \n ${error}`);
     }
